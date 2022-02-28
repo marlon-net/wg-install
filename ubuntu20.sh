d0b00f7393aa65787944464ed0c9c6efe100846f
Marlon Dias <marlon@marlon.net>
2022-02-28 21:29:52 +0000

    FIX net add and improve variables

--- ubuntu20.sh
+++ ubuntu20.sh
@@ -4,10 +4,10 @@
 # - create and set up server
 #   IPv4 and network 10.200.200.0/24
 #   port 51820
-# - create 3 Clients with IPs *.11, *.12, *.13
+# - create Clients with IPs *.11, *.12, *.13 ...
 # Focus on AWS Lightsail and deployed through Terraform 
-# ("sudo" not needed)
-# needs net-tools !
+# ("sudo" not needed ? )
+# needed: curl from net-tools !
 #
 # marlon.net 2022
 # based on https://xalitech.com/wireguard-vpn-server-on-aws-lightsail/
@@ -16,16 +16,19 @@
 sudo apt update -y 
 sudo apt upgrade -y
 
-# useful to get IP information
-#echo "*** utils installation"
-#sudo apt install moreutils -y
-
 # Public IP of Lightsail instance
 curl ifconfig.me/ip > myipv4.txt
 export serverIP=$(cat 'myipv4.txt')
 echo Server IP = ${serverIP}
 
+#----- to do: use parameters
+_NUMBER_OF_CLIENTS = 5
+_WG_LISTEN_PORT = 51820
+_BASE_NET_ADDRESS = "10.200.200" # e.g. "10.200.200" for network 10.200.200.0 - always /24 (to do: improve this)
+
 #-----------------------------------------------------------------------
+# SERVER CONFIGURATION
+
 echo "*** WG installation"
 sudo apt install wireguard -y
 sudo apt install qrencode -y
@@ -37,7 +40,7 @@ mkdir wg && mkdir wg/keys && mkdir wg/clients
 #
 wg genkey | tee wg/keys/${serverIP}_server_private_key  | wg pubkey > wg/keys/${serverIP}_server_public_key
 #
-for COUNTCLIENTS in 1 2 3 4 5
+for COUNTCLIENTS in {1..$_NUMBER_OF_CLIENTS}
 do
     wg genkey | tee wg/keys/${serverIP}_client${COUNTCLIENTS}_private_key | wg pubkey > wg/keys/${serverIP}_client${COUNTCLIENTS}_public_key 
 done
@@ -51,10 +54,10 @@ echo creating [Interface] section
 echo " 
 [Interface]
 PrivateKey = ${serverPrivateKey}
-Address = 10.200.200.1/24
-ListenPort = 51820
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o ens3 -j MASQUERADE; iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o ens3 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o ens3 -j MASQUERADE; iptables -t nat -D POSTROUTING -s 10.200.200.0/24 -o eth0 -j MASQUERADE
+Address = ${_BASE_NET_ADDRESS}.1/24
+ListenPort = ${_WG_LISTEN_PORT}
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o ens3 -j MASQUERADE; iptables -t nat -A POSTROUTING -s ${_BASE_NET_ADDRESS}.0/24 -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o ens3 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o ens3 -j MASQUERADE; iptables -t nat -D POSTROUTING -s ${_BASE_NET_ADDRESS}.0/24 -o eth0 -j MASQUERADE
 SaveConfig = true 
 "| sudo tee /etc/wireguard/wg0.conf
 
@@ -67,12 +70,11 @@ do
     echo "
 [Peer] # client${countFiles}
 PublicKey = $(cat ${FILE})
-AllowedIPs = 10.200.200.1${countFiles}/32
+AllowedIPs = ${_BASE_NET_ADDRESS}.1${countFiles}/24
 "| sudo tee -a /etc/wireguard/wg0.conf
 done
 
 echo $countFiles files found
-
 
 echo "*** bring the Wireguard interface up and makes sure it is auto start on reboot"
 sudo wg-quick up wg0 &&  
@@ -83,7 +85,7 @@ sudo sed -i 's/\#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sudo sysctl -p
 sudo echo 1 | tee /proc/sys/net/ipv4/ip_forward
 
-#-----------------------------------------------------------------------
+#----------------------------------------
 echo "### FIREWALL"
 
 # Track VPN connection
@@ -94,8 +96,8 @@ sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT
 
 # TCP & UDP recursive DNS traffic
-sudo iptables -A INPUT -s 10.200.200.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
-sudo iptables -A INPUT -s 10.200.200.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A INPUT -s ${_BASE_NET_ADDRESS}.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A INPUT -s ${_BASE_NET_ADDRESS}.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 
 # Allow forwarding of packets that stay in the VPN tunnel
 sudo iptables -A FORWARD -i wg0 -o wg0 -m conntrack --ctstate NEW -j ACCEPT
@@ -121,19 +123,21 @@ sudo iptables -L -n -t nat
 
 echo ***** END OF SERVER CONFIGURATION
 
-
+# END OF SERVER CONFIGURATION
 #-----------------------------------------------------------------------
+# CREATING CLIENT FILES (to be copied to each client)
+
 echo "### For each CLIENT"
 
 export countFiles=0
-for FILE in wg/keys/*client*public_key
+for FILE in wg/keys/*client*private_key
 do
-    ((countFiles++))
-    echo creating client ${countFiles}
+    echo found ${FILE}
 
+    ((countFiles++))
     export clientName=client${countFiles}
-    export clientAddress="10.200.200.1${countFiles}/32"
-    export clientPrivateKey=$(cat wg/keys/${serverIP}_${clientName}_private_key)
+    export clientAddress="${_BASE_NET_ADDRESS}.1${countFiles}/24"
+    export clientPrivateKey=$(cat ${FILE})
 
     echo "[Interface] 
 Address = ${clientAddress}
